5b027252d490946d7394be5c1bb155cd4816ec0d
judith <jyouziel20@hotmail.com>
2023-06-25 10:58:10 +0100

    Complete Task 1

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,7 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log("Hello Everyone!");
+console.log(document.body.parentNode);
+console.log(document.getElementById("description").innerHTML);
+console.log(document.querySelector(".about").innerHTML);
+console.log(document.querySelectorAll(".about"));
