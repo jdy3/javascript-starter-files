48f0eec5563bbb4ba3f8f3647cc848dc52fbea9d
judith <jyouziel20@hotmail.com>
2023-06-26 14:37:35 +0100

    Complete session 3 task 1

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,19 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+const name = "Tanya";
+let age = 10;
+
+let a = 5;
+let b = 10;
+let c = a + b;
+
+console.log(c);
+
+a = 20;
+
+console.log(c);
+
+c = a + b;
+
+console.log(c);
