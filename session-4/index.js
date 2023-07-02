0bc5a2b50f930a8a8e451e3bbced42d23e48184c
judith <jyouziel20@hotmail.com>
2023-07-02 10:09:27 +0100

    Complete homework

--- session-4/index.js
+++ session-4/index.js
@@ -16,14 +16,14 @@ const personB = {
 
 const personC = {
   name: "Michelle",
-  age: 17,
+  age: 18,
   location: "Barnet",
   likes: "Swimming",
 };
 
 const personD = {
   name: "Shingui",
-  age: 20,
+  age: 21,
   location: "Manchester",
   likes: "Running",
 };
@@ -37,46 +37,83 @@ console.log(biography(personA));
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-const x = 6;
-const y = 4;
+// const x = 6;
+// const y = 4;
 
-const addition = x + y;
+// const addition = x + y;
 
-console.log("Addition: x + y " + addition);
+// console.log("Addition: x + y " + addition);
 
-const subtraction = x - y;
+// const subtraction = x - y;
 
-console.log("Subtraction: x - y " + subtraction);
+// console.log("Subtraction: x - y " + subtraction);
 
-const multiplication = x * y;
+// const multiplication = x * y;
 
-console.log("multiplication: x * y " + multiplication);
+// console.log("multiplication: x * y " + multiplication);
 
-const division = x * y;
+// const division = x * y;
 
-console.log("Division: x + y " + division);
+// console.log("Division: x + y " + division);
 
-const exponent = x ** y;
+// const exponent = x ** y;
 
-console.log(`Exponent: x**y ${exponent}`);
+// console.log(`Exponent: x**y ${exponent}`);
 
-const remainder = x % y;
+// const remainder = x % y;
 
-console.log(`Remainder: x % y ${remainder}`);
+// console.log(`Remainder: x % y ${remainder}`);
 
-const square = y * y;
+// const square = y * y;
 
-console.log(`Square: y*y ${square}`);
+// console.log(`Square: y*y ${square}`);
 
-let myAge = 19;
-const minDrivingAge = 18;
+// let myAge = 19;
+// const minDrivingAge = 18;
 
-const drive = (age, minAge) => {
-  return age >= minAge
-    ? "Yes, you are old enough to drive!"
-    : "No, you are not old enough to drive.";
-};
+// const drive = (age, minAge) => {
+//   return age >= minAge
+//     ? "Yes, you are old enough to drive!"
+//     : "No, you are not old enough to drive.";
+// };
 
-console.log(drive(myAge, minDrivingAge));
+// console.log(drive(myAge, minDrivingAge));
 
 // ****************************************************TASK 3****************************************************
+
+// Homework
+
+const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+  subtract(num1, num2) {
+    return num1 - num2;
+  },
+  multiply(num1, num2) {
+    return num1 * num2;
+  },
+  divide(num1, num2) {
+    return num1 / num2;
+  },
+};
+
+const whoIsOlder = (person1, person2) => {
+  return person1.age > person2.age
+    ? `${person1.name} is ${calculator.subtract(
+        person1.age,
+        person2.age
+      )} years older that ${person2.name}.`
+    : person2.age > person1.age
+    ? `${person2.name} is ${calculator.subtract(
+        person2.age,
+        person1.age
+      )} years older that ${person1.name}.`
+    : `${person1.name} and ${person2.name} are both ${person1.age} years old.`;
+};
+
+console.log(
+  whoIsOlder(personA, personD),
+  whoIsOlder(personD, personA),
+  whoIsOlder(personB, personC)
+);
