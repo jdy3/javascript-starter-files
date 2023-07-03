4804270dc5bd1330b1950b9781a8c17be99b9c4d
judith <jyouziel20@hotmail.com>
2023-07-03 12:06:21 +0100

    Complete Session 5 Task 2

--- session-5/index.js
+++ session-5/index.js
@@ -1,19 +1,29 @@
 function getGrade(total) {
-  if (total > 90) {
-    console.log(`A+`);
-  } else if (total > 80 && total <= 90) {
-    console.log(`A`);
-  } else if (total > 70 && total <= 80) {
-    console.log(`B`);
-  } else if (total > 60 && total <= 70) {
-    console.log(`C`);
-  } else if (total > 50 && total <= 60) {
-    console.log(`D`);
-  } else if (total > 40 && total <= 50) {
-    console.log(`E`);
-  } else if (total > 30 && total <= 40) {
-    console.log(`F`);
-  } else console.log(`Fail`);
+  switch (true) {
+    case total > 90:
+      console.log(`A+`);
+      break;
+    case total > 80 && total <= 90:
+      console.log(`A`);
+      break;
+    case total > 70 && total <= 80:
+      console.log(`B`);
+      break;
+    case total > 60 && total <= 70:
+      console.log(`C`);
+      break;
+    case total > 50 && total <= 60:
+      console.log(`D`);
+      break;
+    case total > 40 && total <= 50:
+      console.log(`E`);
+      break;
+    case total > 30 && total <= 40:
+      console.log(`F`);
+      break;
+    default:
+      console.log(`Fail`);
+  }
 }
 
 console.log(getGrade(99), getGrade(55), getGrade(15));
