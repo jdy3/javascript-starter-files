00863e786c79ff8564628b260e890cb23f28be1d
judith <jyouziel20@hotmail.com>
2023-07-10 10:43:53 +0100

    Complete Session 7, task 1

--- session-7/index.js
+++ session-7/index.js
@@ -1,38 +1,20 @@
 // ****************************************************TASK 1****************************************************
 
+const animals = ["cat", "dog", "wolf", "lion", "eagle", "zebra"];
 
+animals.pop();
 
-
-
-
-
-
-
-
-
-
-
-
+console.log(animals);
 
 // ****************************************************TASK 2****************************************************
-
-
-
-
-
-
-
-
-
-
 
 // ****************************************************TASK 3****************************************************
 
 // Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
 const people = [
   { name: `t'challa`, age: 37 },
-  { name: 'shuri', age: 16 },
-  { name: 'killmonger', age: 32 },
+  { name: "shuri", age: 16 },
+  { name: "killmonger", age: 32 },
 ];
 
 // This is a helper function that should be used in the final task of the session.
