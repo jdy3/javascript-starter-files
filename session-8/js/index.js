78aead26f9b1e86884d34fec57ccc03ff89b2947
judith <jyouziel20@hotmail.com>
2023-07-11 15:10:04 +0100

    Complete Session 8 Task 1

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,13 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+const button = document.createElement("button");
+
+const content = document.createTextNode("Show Population");
+
+button.appendChild(content);
+
+button.onClick = displayPopulation;
+
+document.body.appendChild(button);
